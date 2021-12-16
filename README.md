c91f8497ddf343ef5f7067b18a55aeb3a9292890
Keith A. Lewis <keithalewis@live.com>
2021-12-16 16:56:45 -0500

    solutions

--- README.md
+++ README.md
@@ -17,13 +17,18 @@ where f is the forward and sigma is the volatility
 
 Solution:
 
+E[F_t] = f since E[B_t] = 0, Var(F_t) = f^2 sigma^2 t since Var(B_t) = t.
+
 Consider a European option that pays (F_T)^2 at time T.
 
 2. Derive a formula for V_t = E_t[F_T^2] in terms of F_t.
 
 Hint. Use B_t^2 - t is a martingale.
 
-Solution:
+E_t[(F_t)^2] = E_t[f^2(1 + 2 sigma B_T + sigma^2 B_T^2)]
+             = f^2 E_t[1 + 2 sigma B_T + sigma^2 (B_T^2 - T + T)]
+             = f^2 (1 + 2 sigma B_t + sigma^2 (B_t^2 - t + T)
+             = f^2 (F_t)^2 + sigma^2 (T - t)
 
 Let Z be a standard normal random variable and N(z) = P(Z <= z) be its cumulative distribution function.
 
@@ -31,21 +36,37 @@ Let Z be a standard normal random variable and N(z) = P(Z <= z) be its cumulativ
 
 Solution:
 
+E[Z 1(Z <= a)] = int_-infty^a z e^{-z^2/2} dz/sqrt(2 pi)
+               = -e^{z^2/2}/sqrt(2 pi) |_-infty^a
+               = -n(a)
+
 4. Prove the forward value of a European put option with strike k and expiration T is
 
     p = E[max{k - F_T, 0}] = (k - f) N(x) + f sigma sqrt(t) n(x)
 
-where x = (k - f)/(sigma sqrt(t)).
+where x = (k - f)/(f sigma sqrt(t)).
 
 Hint. B_t/sqrt(t) is standard normal.
 
 Solution:
+
+    p = E[max{k - F_T, 0}]
+      = E[(k - F_T)1(F_T <= k)]
+      = k N(x) - E[F_t 1(F_t <= k)]
+      = k N(x) - f N(x) + f sigma E[B_t 1(B_t <= (k - f)/f sigma)]
+      = (k - f) N(x) + f sigma sqrt(t) n((k - f)/(f sigma sqrt(t)))
+
+Since E[B_T 1(B_t/sqrt(T) <= x)] = sqrt(T) n(x).
 
 5. Derive a closed form solution for the Bachelier delta dp/df.
 
 Hint. Use dg(F_T)/df = g'(F_T) dF_T/df where g(x) = max{k - x, 0}.
 
 Solution:
+
+    dp/df = E[-1(F_T <= k)(1 + sigma B_T)]
+          = -N(x) - sigma E[B_T 1(B_T/sqrt(T) <= x)]
+          = -N(x) - sigma sqrt(T) n(x)
 
 Coding Exam
 
--- xll_template.cpp
+++ xll_template.cpp
@@ -1,5 +1,7 @@
 ﻿// xll_template.cpp - Sample xll project.
+#include <cassert>
 #include <cmath>
+#include <random>
 #include "xll_template.h"
 
 using namespace xll;
@@ -24,7 +26,9 @@ double N(double x)
 
 double bachelier_put(double f, double sigma, double k, double t)
 {
-	return f + sigma + k + t; // !!! replace with correct formula
+	double x = (k - f) / (f * sigma * sqrt(t));
+
+	return (k - f)*N(x) + f*sigma*sqrt(t)*n(x);
 }
 
 int bachelier_put_test()
@@ -37,6 +41,20 @@ int bachelier_put_test()
 
 	//!!! Write a Monte Carlo simulation that tests the put value.
 	// Use 10,000 simulations and test the error is less than 2*stdev.
+	std::default_random_engine dre;
+	std::normal_distribution<double> Z(0, 1);
+
+	auto X = [f, sigma, k, t, &Z, &dre]() {
+		double srt = sigma * sqrt(t);
+		double F = f * (1 + srt * Z(dre));
+
+		return std::max(k - F, 0.);
+	};
+
+	double p = fms::average(10000, X);
+	double p_ = bachelier_put(f, sigma, k, t);
+
+	assert(fabs(p - p_) < 2 * stdev);
 
 	return 0;
 }
@@ -61,17 +79,53 @@ double WINAPI xll_bachelier_put(double f, double sigma, double k, double t)
 
 double bachelier_put_delta(double f, double sigma, double k, double t)
 {
-	return f + sigma + k + t; // !!! replace with correct formula
+	double srt = sigma * sqrt(t);
+	double x = (k - f) / (f * srt);
+
+	return -N(x) + srt*n(x); 
+}
+
+bool test_delta(double f, double sigma, double k, double t, double h)
+{
+	std::function<double(double)> p = [sigma, k, t](double x) {
+		return bachelier_put(x, sigma, k, t);
+	};
+	auto dp = bachelier_put_delta(f, sigma, k, t);
+
+	return fms::derivative_test(p, f, h, dp, 1.);
 }
 
 // !!! Test bachelier_put_delta using difference quotients for
-// f : {90, 100, 110}
-// sigma : {.1, .2, .3}
-// k : {90, 100, 110}
-// t : {.1, .2, .3}
-// h : {.01, .001, .0001}
+int bachelier_put_delta_test()
+{
+	for (double f : {90, 100, 110})
+		for (double sigma : {.1, .2, .3})
+			for (double k : {90, 100, 110})
+				for (double t : {.1, .2, .3})
+					for (double h : {.01, .001, .0001})
+						assert(test_delta(f, sigma, k, t, h));
 
-// !!! Implement BACHELIER.PUT.DELTA
+	return 0;
+}
+int bachelier_put_delta_test_ = bachelier_put_delta_test();
+
+
+AddIn xai_bachelier_put_delta(
+	Function(XLL_DOUBLE, "xll_bachelier_put_delta", "BACHELIER.PUT.DELTA")
+	.Arguments({
+		Arg(XLL_DOUBLE, "f", "is the forward."),
+		Arg(XLL_DOUBLE, "sigma", "is the volatility."),
+		Arg(XLL_DOUBLE, "k", "is the strike."),
+		Arg(XLL_DOUBLE, "t", "is the time in years to expriation."),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp("Forward value of a Bachelier put option.")
+);
+double WINAPI xll_bachelier_put_delta(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+	return bachelier_put_delta(f, sigma, k, t);
+}
 
 // !!! Create a spreadsheet with a graph of put value and put delta as a function of strike k.
 // Use f = 100, sigma = 0.2, t = 0.25, and k = 80, 81, ..., 120.
